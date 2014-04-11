602fe5eccf137f24aae1dccb3343b4ad26fd61da
Allie Ai <aial@gse.harvard.edu>
2014-04-10 20:50:10 -0400

    vis 2 prototype

---
+++ votes-by-subject.js
@@ -0,0 +1,532 @@
+var dataSet = [
+        {
+            "bill_id": "hr4015-113",
+            "chamber": "senate",
+            "congress": 113,
+            "number": 134,
+            "question": "On Motion to Recommit with Instructions -- H.R. 4015 -- To amend title XVIII of the Social Security Act to repeal the Medicare sustainable growth rate and improve Medicare payments for physicians and other professionals, and for other purposes",
+            "required": "1/2",
+            "result": "Failed",
+            "roll_id": "h134-2014",
+            "roll_type": "On Motion to Recommit with Instructions",
+            "source": "http://clerk.house.gov/evs/2014/roll134.xml",
+            "url": "http://clerk.house.gov/evs/2014/roll134.xml",
+            "vote_type": "recommit",
+            "voted_at": "2014-03-14T15:07:00Z",
+            "year": 2014,
+            "party": "R"
+        },
+        {
+            "bill_id": "hr4015-113",
+            "chamber": "senate",
+            "congress": 113,
+            "number": 135,
+            "question": "On Passage -- H.R. 4015 -- To amend title XVIII of the Social Security Act to repeal the Medicare sustainable growth rate and improve Medicare payments for physicians and other professionals, and for other purposes",
+            "required": "1/2",
+            "result": "Passed",
+            "roll_id": "h135-2014",
+            "roll_type": "On Passage",
+            "source": "http://clerk.house.gov/evs/2014/roll135.xml",
+            "url": "http://clerk.house.gov/evs/2014/roll135.xml",
+            "vote_type": "passage",
+            "voted_at": "2014-03-14T15:15:00Z",
+            "year": 2014,
+            "party": "D"
+        },
+        {
+            "bill_id": "hres515-113",
+            "chamber": "senate",
+            "congress": 113,
+            "number": 125,
+            "question": "On Ordering the Previous Question -- H. Res. 515 -- Providing for consideration of H.R. 3189, the Water Rights Protection Act; and providing for consideration of H.R. 4015, the SGR Repeal and Medicare Provider Payment Modernization Act of 2014",
+            "required": "1/2",
+            "result": "Passed",
+            "roll_id": "h125-2014",
+            "roll_type": "On Ordering the Previous Question",
+            "source": "http://clerk.house.gov/evs/2014/roll125.xml",
+            "url": "http://clerk.house.gov/evs/2014/roll125.xml",
+            "vote_type": "other",
+            "voted_at": "2014-03-13T18:04:00Z",
+            "year": 2014,
+            "party": "D"
+        },
+        {
+            "bill_id": "hres515-113",
+            "chamber": "senate",
+            "congress": 113,
+            "number": 126,
+            "question": "On Agreeing to the Resolution -- H. Res. 515 -- Providing for consideration of H.R. 3189, the Water Rights Protection Act; and providing for consideration of H.R. 4015, the SGR Repeal and Medicare Provider Payment Modernization Act of 2014",
+            "required": "1/2",
+            "result": "Passed",
+            "roll_id": "h126-2014",
+            "roll_type": "On Agreeing to the Resolution",
+            "source": "http://clerk.house.gov/evs/2014/roll126.xml",
+            "url": "http://clerk.house.gov/evs/2014/roll126.xml",
+            "vote_type": "passage",
+            "voted_at": "2014-03-13T18:23:00Z",
+            "year": 2014,
+            "party": "D"
+        },
+        {
+            "bill_id": "hr3631-111",
+            "chamber": "house",
+            "congress": 111,
+            "number": 737,
+            "question": "On Motion to Suspend the Rules and Pass -- H.R. 3631 -- Medicare Premium Fairness Act",
+            "required": "2/3",
+            "result": "Passed",
+            "roll_id": "h737-2009",
+            "roll_type": "On Motion to Suspend the Rules and Pass",
+            "source": "http://clerk.house.gov/evs/2009/roll737.xml",
+            "url": "http://clerk.house.gov/evs/2009/roll737.xml",
+            "vote_type": "passage",
+            "voted_at": "2009-09-24T16:45:00Z",
+            "year": 2009,
+            "party": "D"
+        },
+        {
+            "bill_id": "hres903-111",
+            "chamber": "house",
+            "congress": 111,
+            "number": 881,
+            "question": "On Ordering the Previous Question -- H. Res. 903 -- Providing for consideration of the bill (H.R. 3962) to provide affordable, quality health care for all Americans and reduce the growth in health care spending, and for other purposes, and (H.R. 3961) to amend title XVIII of the Social Security Act of reform the Medicare SGR payment system for physicians",
+            "required": "1/2",
+            "result": "Passed",
+            "roll_id": "h881-2009",
+            "roll_type": "On Ordering the Previous Question",
+            "source": "http://clerk.house.gov/evs/2009/roll881.xml",
+            "url": "http://clerk.house.gov/evs/2009/roll881.xml",
+            "vote_type": "other",
+            "voted_at": "2009-11-07T18:27:00Z",
+            "year": 2009,
+            "party": "D"
+        },
+        {
+            "bill_id": "hres903-111",
+            "chamber": "house",
+            "congress": 111,
+            "number": 882,
+            "question": "On Agreeing to the Resolution -- H. Res. 903 -- Providing for consideration of the bill (H.R. 3962) to provide affordable, quality health care for all Americans and reduce the growth in health care spending, and for other purposes, and (H.R. 3961) to amend title XVIII of the Social Security Act of reform the Medicare SGR payment system for physicians",
+            "required": "1/2",
+            "result": "Passed",
+            "roll_id": "h882-2009",
+            "roll_type": "On Agreeing to the Resolution",
+            "source": "http://clerk.house.gov/evs/2009/roll882.xml",
+            "url": "http://clerk.house.gov/evs/2009/roll882.xml",
+            "vote_type": "passage",
+            "voted_at": "2009-11-07T18:44:00Z",
+            "year": 2009,
+            "party": "D"
+        },
+        {
+            "bill_id": "hr3961-111",
+            "chamber": "house",
+            "congress": 111,
+            "number": 907,
+            "question": "Table Appeal of the Ruling of the Chair -- H.R. 3961 -- Medicare Physician Payment Reform Act of 2009",
+            "required": "1/2",
+            "result": "Passed",
+            "roll_id": "h907-2009",
+            "roll_type": "Table Appeal of the Ruling of the Chair",
+            "source": "http://clerk.house.gov/evs/2009/roll907.xml",
+            "url": "http://clerk.house.gov/evs/2009/roll907.xml",
+            "vote_type": "other",
+            "voted_at": "2009-11-19T20:53:00Z",
+            "year": 2009,
+            "party": "R"
+        },
+        {
+            "bill_id": "hr3961-111",
+            "chamber": "house",
+            "congress": 111,
+            "number": 908,
+            "question": "On Motion to Recommit with Instructions -- H.R. 3961 -- Medicare Physician Payment Reform Act of 2009",
+            "required": "1/2",
+            "result": "Failed",
+            "roll_id": "h908-2009",
+            "roll_type": "On Motion to Recommit with Instructions",
+            "source": "http://clerk.house.gov/evs/2009/roll908.xml",
+            "url": "http://clerk.house.gov/evs/2009/roll908.xml",
+            "vote_type": "recommit",
+            "voted_at": "2009-11-19T21:22:00Z",
+            "year": 2009,
+            "party": "R"
+        },
+        {
+            "bill_id": "hr3961-111",
+            "chamber": "house",
+            "congress": 111,
+            "number": 909,
+            "question": "On Passage -- H.R. 3961 -- Medicare Physician Payment Reform Act of 2009",
+            "required": "1/2",
+            "result": "Passed",
+            "roll_id": "h909-2009",
+            "roll_type": "On Passage",
+            "source": "http://clerk.house.gov/evs/2009/roll909.xml",
+            "url": "http://clerk.house.gov/evs/2009/roll909.xml",
+            "vote_type": "passage",
+            "voted_at": "2009-11-19T21:29:00Z",
+            "year": 2009,
+            "party": "D"
+        },
+        {
+            "bill_id": "hr3961-111",
+            "chamber": "house",
+            "congress": 111,
+            "number": 67,
+            "question": "On Motion to Concur in Senate Amendments -- H.R. 3961 -- Medicare Physician Payment Reform Act",
+            "required": "1/2",
+            "result": "Passed",
+            "roll_id": "h67-2010",
+            "roll_type": "On Motion to Concur in Senate Amendments",
+            "source": "http://clerk.house.gov/evs/2010/roll067.xml",
+            "url": "http://clerk.house.gov/evs/2010/roll067.xml",
+            "vote_type": "passage",
+            "voted_at": "2010-02-26T00:26:00Z",
+            "year": 2010,
+            "party": "R"
+        },
+        {
+            "bill_id": "hr4994-111",
+            "chamber": "house",
+            "congress": 111,
+            "number": 626,
+            "question": "On Motion to Suspend the Rules and Concur in the Senate Amendments -- H.R. 4994 -- Medicare and Medicaid Extenders Act",
+            "required": "2/3",
+            "result": "Passed",
+            "roll_id": "h626-2010",
+            "roll_type": "On Motion to Suspend the Rules and Concur in the Senate Amendments",
+            "source": "http://clerk.house.gov/evs/2010/roll626.xml",
+            "url": "http://clerk.house.gov/evs/2010/roll626.xml",
+            "vote_type": "passage",
+            "voted_at": "2010-12-09T18:09:00Z",
+            "year": 2010,
+            "party": "D"
+        },
+        {
+            "bill_id": "hres501-112",
+            "chamber": "house",
+            "congress": 112,
+            "number": 949,
+            "question": "On Agreeing to the Resolution -- H. Res. 501 -- Expressing the sense of the House of Representatives regarding any final measure to extend the payroll tax holiday, extend Federally funded unemployment insurance benefits, or prevent decreases in reimbursement for physicians who provide care to Medicare beneficiaries",
+            "required": "1/2",
+            "result": "Passed",
+            "roll_id": "h949-2011",
+            "roll_type": "On Agreeing to the Resolution",
+            "source": "http://clerk.house.gov/evs/2011/roll949.xml",
+            "url": "http://clerk.house.gov/evs/2011/roll949.xml",
+            "vote_type": "passage",
+            "voted_at": "2011-12-20T21:16:00Z",
+            "year": 2011,
+            "party": "R"
+        },
+        {
+            "bill_id": "hr1845-112",
+            "chamber": "house",
+            "congress": 112,
+            "number": 634,
+            "question": "On Motion to Suspend the Rules and Pass, as Amended -- H.R. 1845 -- To provide for a study on issues relating to access to intravenous immune globulin (IVIG) for Medicare beneficiaries in all care settings and a demonstration project to examine the benefits of providing coverage and payment for items and services necessary to administer IVIG in the home",
+            "required": "2/3",
+            "result": "Passed",
+            "roll_id": "h634-2012",
+            "roll_type": "On Motion to Suspend the Rules and Pass, as Amended",
+            "source": "http://clerk.house.gov/evs/2012/roll634.xml",
+            "url": "http://clerk.house.gov/evs/2012/roll634.xml",
+            "vote_type": "passage",
+            "voted_at": "2012-12-19T19:28:00Z",
+            "year": 2012,
+            "party": "R"
+        }
+    ];
+
+var subjectNode = {
+    "bill_id": "Medicare"
+}
+
+var margin = {
+    top: 50,
+    right: 50,
+    bottom: 50,
+    left: 50
+};
+
+var width = 560 - margin.left - margin.right;
+var height = 400 - margin.bottom - margin.top;
+var centered; 
+var detailVisWidth = 350;
+var detailVisHeight = 350;
+
+var bbVis = {
+    x: 100,
+    y: 10,
+    w: width - 100,
+    h: 300
+};
+
+var detailVis = d3.select("#detailVis").append("svg").attr({
+    width:detailVisWidth,
+    height:detailVisHeight
+});
+
+var canvas = d3.select("#vis").append("svg").attr({
+    width: width + margin.left + margin.right,
+    height: height + margin.top + margin.bottom
+    });
+
+var svg = canvas.append("g").attr({
+        transform: "translate(" + margin.left + "," + margin.top + ")"
+    }),
+
+    groups = {
+      SD: "Senate Democrat",
+      HD: "House Democrat",
+      SR: "Senate Republican",
+      HR: "House Republican"
+    },
+
+    colors = {
+      SD: "#4D94DB",
+      HD: "#0066CC",
+      SR: "#E06666",
+      HR: "#CC0000"
+    },
+
+  color = d3.scale.ordinal()
+        .domain([groups.SD, groups.HD, groups.SR, groups.HR])
+        .range([colors.SD, colors.HD, colors.SR, colors.HR]);
+
+var graph = {nodes:[], links:[]};
+
+//load test data
+loadVotes();
+
+for(j = 0; j < graph.nodes.length; j++) {
+      graph.links.push({"source": 0, "target": j}); //links from one node, simplifies the subject, not actual votes on the subject
+}
+
+graph.nodes.unshift(subjectNode);
+
+console.log(graph.nodes);
+console.log(graph.links);
+
+var force = d3.layout.force()
+    .size([width, height])
+    .charge(-50)
+    .linkDistance(100)
+    .on("tick", tick)
+    .on("start", function(d) {})
+    .on("end", function(d) {})
+
+var link = svg.selectAll(".link")
+              .data(graph.links)
+              .enter().append("line")
+              .attr("class", "link");
+
+var node = svg.selectAll(".node")
+              .data(graph.nodes)
+              .enter()
+              .append("g").attr("class", "node");
+
+node.append("circle")
+    .attr("r", 5)
+    .style("fill", function(d) { 
+      if(d.bill_id == "Medicare") return "#F0F0F5" ; //this is the fake node representing the subject
+      
+      var p= d.party, c = d.chamber;
+      var clr; 
+      if(p == "R" &&  c == "senate") clr = groups.SR;
+      else if(p == "R" && c == "house") clr = groups.HR;
+      else if(p == "D" && c == "senate") clr = groups.SD;
+      else if(p == "D" && c == "house") clr = groups.HD;
+
+      return color(clr); 
+    });
+
+node.append("svg:text")
+        .attr("class", "tooltip")
+        .attr("dx", 12)
+        .attr("dy", ".35em")
+        .text(function(d) { return d.bill_id });
+
+
+force_layout();
+
+
+function tick(d) {
+  graph_update(0);
+}
+
+function force_layout() {
+ force.nodes(graph.nodes)
+      .links(graph.links)
+      .start();
+}
+
+function graph_update(delay) {
+
+  link.transition().duration(delay)
+      .attr("x1", function(d) { return d.target.x; })
+      .attr("y1", function(d) { return d.target.y; })
+      .attr("x2", function(d) { return d.source.x; })
+      .attr("y2", function(d) { return d.source.y; });
+
+  node.transition().duration(delay)
+      .attr("transform", function(d) { 
+        return "translate("+d.x+","+d.y+")"; 
+      });
+}
+
+function loadVotes() {
+  graph.nodes = dataSet;
+}
+
+function loadStations() {
+    d3.csv("../data/NSRDB_StationsMeta.csv",function(error,data){
+      var coords = [];
+        svg.selectAll("circle")
+           .data(data)
+           .enter()
+           .append("circle")
+           .attr("id", function(d){return d.USAF})
+           .attr("cx", function(d) {
+                  coords = projection([d.NSRDB_LON, d.NSRDB_LAT]);
+                  return (coords)? coords[0]:"-100"; //for stations without coords, put them outside the map; there seems to be 11 of them in .csv file
+           })
+           .attr("cy", function(d) {
+                  coords = projection([d.NSRDB_LON, d.NSRDB_LAT]);
+                  return (coords)? coords[1]:"-100"; //for stations without coords, put them outside the map; there seems to be 11 of them in .csv file
+           })
+           .attr("r", function(d){ 
+              if(dataSet[d.USAF]) return stationScale(dataSet[d.USAF].sum); 
+              else return 2 //set stations with no data with r=2
+           })
+           .attr("class", "station")
+           .style("fill", function(d){ 
+              if(dataSet[d.USAF]) return "#0066CC"; 
+              else return "#C0C0C0" //set stations with no data as grey dots #C0C0C0
+           })
+           .on("mouseover", function(d,i) {
+            var mouse = d3.mouse(svg.node()).map( function(d) { return parseInt(d); } );
+
+            tooltip
+              .classed("hidden", false)
+              .attr("style", "left:" + (mouse[0]+10) + "px; top:" + (mouse[1]+7) +"px")
+              .html(d.STATION + ", " + d.ST + "<br/>Sum : " + dataSet[d.USAF].sum)
+          })
+          .on("mouseout",  function(d,i) {
+            tooltip.classed("hidden", true)
+          })
+          .on("click", function(d){
+            createDetailVis(d);
+          });
+    });
+}
+
+
+function loadStats() {
+    d3.json("../data/reducedMonthStationHour2003_2004.json", function(error,data){
+        dataSet = data;
+
+        //set station size scale domain 
+        var max = d3.entries(data)
+            .sort(function(a, b) { return b.value.sum - a.value.sum; })
+            [0].value.sum;
+
+        stationScale.domain([0, max]);
+                
+        loadStations();
+    })
+}
+
+//Main
+/*
+d3.json("../data/us-named.json", function(error, data) {
+
+    var usMap = topojson.feature(data,data.objects.states).features;
+
+    svg.append("g").attr("id", "countries");
+
+    svg.selectAll("path").data(usMap) 
+    .enter()
+    .append("path")
+    .attr("d", path)
+    .attr("class", "country")
+    .on("click", zoomToBB);
+
+    svg.append("path")
+      .datum(topojson.mesh(data, data.objects.states, function(a, b) { return a !== b; }))
+      .attr("id", "state-borders")
+      .attr("d", path);
+
+    tooltip = d3.select("#vis").append("div")
+    .attr("class", "tooltip");
+
+    loadStats();
+});
+*/
+
+var createDetailVis = function(data){
+
+  var stationHourlyData =  dataSet[data.USAF].hourly; 
+
+  // clean the previous graph first
+  detailVis.select("g.chart").remove();
+
+  // create a new graph
+  var x = d3.scale.ordinal()
+      .rangeRoundBands([0, detailVisWidth - margin.left - margin.right], .1);
+
+  var y = d3.scale.linear()
+      .range([detailVisHeight - margin.top - margin.bottom, 0]);
+
+  var timeVals = Object.getOwnPropertyNames(hours).map(function(key) {
+      return parseHour(hours[key]);
+  });
+  x.domain(timeVals);
+
+  var yMax = d3.entries(stationHourlyData)
+            .sort(function(a, b) { return b.value - a.value; })
+            [0].value;
+  y.domain([0, yMax]);
+
+  var xAxis = d3.svg.axis()
+      .scale(x)
+      .orient("bottom")
+      .tickFormat(d3.time.format("%I:%M:%S %p"));
+
+  var yAxis = d3.svg.axis()
+      .scale(y)
+      .orient("right");
+
+  var svgDetail = detailVis.append("g")
+    .attr("class", "chart")
+    .attr('transform', 'translate(' + (margin.left - 30) + ', ' + (margin.top - 30) + ')');
+
+  svgDetail.append("g")
+      .attr("class", "x axis")
+      .attr('transform', 'translate(0, ' + (detailVisHeight - margin.top - margin.bottom) + ')')
+      .call(xAxis)
+      .selectAll("text")
+      .style("text-anchor", "end")
+      .attr("dx", "-.8em")
+      .attr("dy", "-.55em")
+      .attr("transform", "rotate(-75)");
+
+  svgDetail.append("g")
+      .attr("class", "y axis")
+      .attr('transform', 'translate(' + (detailVisWidth - margin.left - margin.right) + ', 0)') //move yaxis to the right
+      .call(yAxis);
+
+  svgDetail.selectAll("rect")
+      .data(d3.entries(stationHourlyData))
+      .enter().append("rect")
+      .attr("x", function(d) { return x(parseHour(d.key)); })
+      .attr("width", x.rangeBand())
+      .attr("y", function(d) { return y(d.value); })
+      .attr("height", function(d) { return detailVisHeight - margin.top - margin.bottom - y(d.value); });
+}
+
+
+
+
+
